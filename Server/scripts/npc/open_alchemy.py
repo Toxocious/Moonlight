9e39090cca45dae2b6e8eb1ea300f45238b60813
Gerardo Cornejo <gerardo_cornejo@icloud.com>
2025-09-24 11:05:14 -0500

    Fix scripts for professions

--- Server/scripts/npc/open_alchemy.py
+++ Server/scripts/npc/open_alchemy.py
@@ -3,6 +3,6 @@ from net.swordie.ms.enums import UIType
 ALCHEMY_CRAFT_SKILL = 92040000
 
 if sm.hasSkill(ALCHEMY_CRAFT_SKILL):
-    sm.openUI(UIType.UI_MAKING_SKILL)
+    sm.openUI(UIType.MAKING_SKILL)
 else:
     sm.systemMessage("Only Alchemists can use this.")
