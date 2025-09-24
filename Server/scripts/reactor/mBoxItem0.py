2718fa6142bfb037d583ae3837a3766a1c009cfa
Gerardo Cornejo <gerardo_cornejo@icloud.com>
2025-09-24 11:21:03 -0500

    Fix for reactors

--- Server/scripts/reactor/mBoxItem0.py
+++ Server/scripts/reactor/mBoxItem0.py
@@ -1,4 +1,5 @@
 # Maple Road - Inside the Dangerous Forest/South Perry Reactor
+import random
 
 # Reactor Tutorial Maple Island Explorer
 DANGEROUS_FOREST = 4000014
@@ -9,7 +10,9 @@ SOUTH_PERRY = 4000030
 FLUSTERED_EXPLORER = 32213
 BOAT_BOARDING_TICKET = 4033914
 
+SMALL_FOREST = 912060100
 KINDLING = 4032980  # Quest Item for Cannoneer Intro
+FIREWORRD_FOR_THE_BONFIRE_QUEST = 2570
 
 reactor.incHitCount()
 reactor.increaseState()
@@ -21,11 +24,16 @@ x, y = sm.getPosition(objectID).getX(), sm.getPosition(objectID).getY()
 
 if sm.getFieldID() == DANGEROUS_FOREST:
     sm.dropItem(FIRST_EXPLORER_GIFT_BOX, x, y)
-elif sm.getFieldID() == SOUTH_PERRY:
-    if sm.hasQuest(FLUSTERED_EXPLORER):
-        if not sm.hasItem(BOAT_BOARDING_TICKET):
-            sm.dropItem(BOAT_BOARDING_TICKET, x, y)
-else:
+elif sm.getFieldID() == SOUTH_PERRY and sm.hasQuest(FLUSTERED_EXPLORER) and not sm.hasItem(BOAT_BOARDING_TICKET):
+    sm.dropItem(BOAT_BOARDING_TICKET, x, y)
+elif sm.getFieldID() == SMALL_FOREST and sm.hasQuest(FIREWORRD_FOR_THE_BONFIRE_QUEST) and not sm.hasItem(
+        BOAT_BOARDING_TICKET):
     sm.dropItem(KINDLING, x, y)
+else:
+    # Get random value against 10 - 100
+    meso = random.randint(10, 500)
+    chr.getField().dropMeso(meso, x, y, x, y + 20)
+    sm.dropItem(2010009, x, y)
 
 sm.removeReactor()
+sm.dispose()
--- Server/scripts/reactor/periFlower0.py
+++ Server/scripts/reactor/periFlower0.py
@@ -1,4 +1,5 @@
 # fire flower in perion mountains
 
-sm.dropItem(4033051, sm.getObjectPositionX(), sm.getObjectPositionY())
+sm.dropItem(4033051, reactor.getPosition().getX(), reactor.getPosition().getY())
 sm.removeReactor()
+sm.dispose()
